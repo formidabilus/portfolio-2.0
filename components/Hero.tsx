e49b6acbbd5a99f746141796c530b9307a12f028
formidabilus <chiriac_razvan@ymail.com>
2023-02-12 19:03:51 +0200

    added BackgroundCircles.tsx component and styling for it, added more styling in Hero.tsx, added profile picture

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import profilePicture from "../public/profile_avatar.jpg";
 
 type Props = {};
 
@@ -16,12 +19,28 @@ const Hero = (props: Props) => {
   });
 
   return (
-    <div>
-      <h1></h1>
-      <span>
-        {text}
-        <Cursor cursorColor="red" />
-      </span>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <div className="relative w-32 h-32  mx-auto">
+        <Image
+          src={profilePicture}
+          className="rounded-full object-cover"
+          alt="profile"
+          layout="fill"
+          priority
+        />
+      </div>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3 ">
+            {text}
+            <Cursor cursorColor="#F7AB0A" />
+          </span>
+        </h1>
+      </div>
     </div>
   );
 };
