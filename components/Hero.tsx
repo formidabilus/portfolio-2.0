e6209b3e181d64041bbbbb8f387f97edcc21896e
formidabilus <chiriac_razvan@ymail.com>
2023-02-13 14:02:14 +0200

    added buttons and styling in Hero and changed favicon

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,17 +3,13 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import profilePicture from "../public/profile_avatar.jpg";
+import Link from "next/link";
 
 type Props = {};
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
-    words: [
-      "Hi, my name is Razvan...",
-      "and...",
-      "I am the Developer you need!",
-      "You can stop the search now.",
-    ],
+    words: ["Hi, my name is #Razvan", "&&", "I am the <Developer/> you need!"],
     loop: true,
     delaySpeed: 2000,
   });
@@ -21,7 +17,7 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <div className="relative w-32 h-32  mx-auto">
+      <div className="relative w-32 h-32 mx-auto">
         <Image
           src={profilePicture}
           className="rounded-full object-cover"
@@ -30,16 +26,30 @@ const Hero = (props: Props) => {
           priority
         />
       </div>
-      <div>
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Software Developer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h2 className="text-5xl lg:text-6xl font-semibold px-10">
           <span className="mr-3 ">
             {text}
             <Cursor cursorColor="#F7AB0A" />
           </span>
-        </h1>
+        </h2>
+        <div className="pt-5 bg-transparent">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,28 @@ const Home: NextPage = () => {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Razvan Chiriac</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header />
 
