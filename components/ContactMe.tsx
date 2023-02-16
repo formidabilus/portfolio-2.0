5a8fc8a1957e359adc58644221aecf939f807155
formidabilus <chiriac_razvan@ymail.com>
2023-02-16 15:28:11 +0200

    added error handling in form from ContactMe component

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
+type FormInputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 const ContactMe = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (formData) =>
+    console.log(formData);
+
   return (
     <div className="h-screen relative flex flex-col justify-evenly items-center text-center mx-auto md:text-left md:flex-row max-w-7xl px-10">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -35,14 +52,52 @@ const ContactMe = (props: Props) => {
           </div>
         </div>
 
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="text" />
+            <div>
+              <input
+                {...register("name", { required: true })}
+                placeholder="Name"
+                className="contactInput"
+                type="text"
+              />
+              <p className="text-red-700">
+                {errors.name && "Please enter your name"}
+              </p>
+            </div>
+            <div>
+              <input
+                {...register("email", { required: true })}
+                placeholder="Email"
+                className="contactInput"
+                type="email"
+              />
+              <p className="text-red-700">
+                {errors.email && "Please enter your email"}
+              </p>
+            </div>
           </div>
-          <input placeholder="Subject" className="contactInput" type="text" />
+          <input
+            {...register("subject", { required: true })}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <p className="text-red-700">
+            {errors.subject && "Please type a subject"}
+          </p>
 
-          <textarea placeholder="Message" className="contactInput" />
+          <textarea
+            {...register("message", { required: true })}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <p className="text-red-700">
+            {errors.message && "Please enter a message"}
+          </p>
           <button
             type="submit"
             className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
