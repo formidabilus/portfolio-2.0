74dd5db4efc2d6d2e92964b895801f276b9bd79d
formidabilus <chiriac_razvan@ymail.com>
2023-02-23 03:35:51 +0200

    changed hight on card in WorkExperience

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -25,7 +25,7 @@ const WorkExperience = ({ experiences }: Props) => {
         Experience
       </h3>
 
-      <div className="w-full md:h-[80%] flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory  scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="w-full md:h-[70%] flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory  scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
         {experiences?.map((experience) => (
           <ExperienceCard key={experience._id} experience={experience} />
         ))}
